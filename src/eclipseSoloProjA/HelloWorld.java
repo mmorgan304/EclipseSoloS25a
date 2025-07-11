e3e2a8a67cff4de8f0e819b586754c473a105289
Morgan <melily@UMICH.EDU>
2025-07-11 15:27:13 -0400

    Programmer2 makes some more changes.

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("here is another println");
-		System.out.println("Programmer 2 did this");
+		System.out.println("Programmer 2 did this. I expected Programmer 1 to make changes here.");
 	}
 
 }
