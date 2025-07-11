9f6d2371af0c31031885661b1ddcafbc5fc09f81
Morgan <melily@UMICH.EDU>
2025-07-11 15:23:41 -0400

    Programmer 1 makes a conflict.

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("here is another println");
-		System.out.println("Programmer 2 did this");
+		System.out.println("Programmer 2 did this. Programmer 1 and definitely not an impostor did this.");
 	}
 
 }
